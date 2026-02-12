a6117149707515a670a96ad92ab6f51f2847968c
Gion Desax <gion.desax@script-source.net>
2026-02-12 20:57:51 +0100

    Add IDE configuration files, argparse examples, and enhanced text parsing scripts

--- script.py
+++ script.py
@@ -15,3 +15,30 @@
 # Wörter: 5
 
 # Optional: Erweitern Sie das Skript, um auch die Anzahl der Vokale und Konsonanten zu zählen.
+import argparse
+
+from script_loesung import characters, sum_consonants
+
+parser = argparse.ArgumentParser(description="Dies ist nur ein kleiner Textlese Bot")
+
+parser.add_argument("text", type=str, help="Text eingeben")
+parser.add_argument("--details", action="store_true", help="Anzahl der Wörter, Vokalen und Konsonanten")
+
+args = parser.parse_args()
+
+text = args.text
+chars = len(text)
+words = len(text.split())
+
+print(f"Anzahl Zeichen im Text: {chars}")
+if args.details:
+    print(f"Anzahl Wörter: {words}")
+
+    vowels = "aeiouAEIOU"
+    sum_vowels = sum(1 for char in text if char in vowels)
+    print(f"Anzahl Vokale im Text: {sum_vowels}")
+
+    consonants = "bcdfghjklmnpqrstvwxyz"
+    sum_consonants = sum(1 for char in text if char.lower() in consonants)
+    print(f"Anzahl Consonants im Text: {sum_consonants}")
+
